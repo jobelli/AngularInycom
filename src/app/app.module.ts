82e7f488b139a9c1d9f4a55bfbf7343805bb4f63
jobelli <jrgbella8@gmail.com>
2023-02-08 18:07:22 +0100

    practica 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { ImageModule } from 'primeng/image';
+import { InputTextModule } from 'primeng/inputtext';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToolbarModule } from 'primeng/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 import { HomeComponent } from './home/home.component';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HomeComponent, CatalogoComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ToolbarModule,
+        SplitButtonModule,
+        ButtonModule,
+        ReactiveFormsModule,
+        InputTextModule,
+        FormsModule,
+        DividerModule,
+        ImageModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public formulario!: FormGroup;
+  public visualizarFormulario: boolean = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+      this.formulario = this.fb.group({
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', Validators.required],
+      });
+  }
+
+  onSubmit() {
+      console.log(this.formulario.value);
+  }
+
+  onRegistrarse() {
+      console.log('registrando');
+  }
 
 }
